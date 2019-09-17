487f2877b37a2c85aa7e481d17610d6f16885c79
Heizi <35397814+ElisaMin@users.noreply.github.com>
2019-09-18 00:16:58 +0800

    Add files via upload

--- software/js/color.js
+++ software/js/color.js
@@ -1,81 +1,32 @@
-var mq="3"/*菜单列表数量+1*/,cq="3"/*科目数量+1*/;
 function getCookie(cname) {var name = cname + "=";var ca = document.cookie.split(';');for(var i = 0; i < ca.length; i++) {var c = ca[i];while (c.charAt(0) == ' ') {c = c.substring(1);}if (c.indexOf(name)  == 0) {return c.substring(name.length, c.length);}}return "";
-};function setstyle(){
-/*var color=getCookie("color");
-	for (var i=1;i<cq;i++){
-		console.log(color);
-		if (color=="color"+i){
-			var do ;
-			do = setcolor+i+"()";
-			console.log(do);
-			}
-	}; */
-};function setcolor0(){
+};var style=getCookie("style");console.log(style);function setstyle() {if (style==1){setstyle1();} else if (style==2){setstyle2();} else {setstyle0();};};
+
+var mq="3"/*菜单列表数量+1*/,cq="3"/*科目数量+1*/;
+
+function setstyle0(){
 	var manuid="0";
-/*菜单变换*/
-	for (var i=0;i<mq;i++){
-		var x="mc"+i;
-		console.log(x);
-		document.getElementById(x).className="ml";
-	};
-	var menuid="mc"+manuid;
-	document.getElementById(menuid).className="ml active";
-//科目颜色
-	//这是卡的改色的脚本部分，"lightgray"可以改成任意你喜欢的颜色。
-	document.getElementById("class1").style.background="cadetblue";
-	//document.getElementById("class2").style.background="mediumvioletred";
-	//其他颜色
-	var x=document.getElementsByClassName("timelist");
-	for (var i=0;i<x.length;i++){
-		x[i].style.background="#fff";x[i].style.color="#000";
-		};
-	var x=document.getElementsByClassName("card");
-	for (var i=0;i<x.length;i++){
-		x[i].style.color="#fff";
-		};var s=getCookie("color");
-	setstyle();
+		for (var i=0;i<mq;i++){var x="mc"+i;document.getElementById(x).className="ml";};var menuid="mc"+manuid;document.getElementById(menuid).className="ml active";
+		var c="info";var x=document.getElementsByClassName(c);
+		for (var i=0;i<x.length;i++){
+			x[i].style.background="badcolor";x[i].style.color="badcolor";
+		};document.cookie="style=0";
 };
-function setcolor1() {
-	var manuid="1";document.cookie="color=color1";
-//菜单变换
-	for (var i=0;i<mq;i++){
-		var x="mc"+i;
-		console.log(x);
-		document.getElementById(x).className="ml";
-	};
-	var menuid="mc"+manuid;
-	document.getElementById(menuid).className="ml active";
-//科目颜色
-	for (var i=1;i<cq;i++){
-		var x="class"+i;console.log(x);
-		document.getElementById(x).style.color="#000";
-		document.getElementById(x).style.background="#fff";
+
+function setstyle1(){
+	var manuid="1";
+		for (var i=0;i<mq;i++){var x="mc"+i;document.getElementById(x).className="ml";};var menuid="mc"+manuid;document.getElementById(menuid).className="ml active";
+		var c="info";var x=document.getElementsByClassName(c);
+		for (var i=0;i<x.length;i++){
+			x[i].style.background="#fff";x[i].style.color="#000";
 		};
-//其他颜色
-	var x=document.getElementsByClassName("timelist");
-	for (var i=0;i<x.length;i++){
-		x[i].style.background="#fff";x[i].style.color="#000";
-		};	var x=document.getElementsByClassName("card");
-	for (var i=0;i<x.length;i++){
-		x[i].style.color="#000";
-		};
+	document.cookie="style=1";
 };
-function setcolor2() {
-	var manuid="2";document.cookie="color=color2";
-	for (var i=0;i<mq;i++){
-		var x="mc"+i;
-		console.log(x);
-		document.getElementById(x).className="ml";
-	};
-	var menuid="mc"+manuid;
-	document.getElementById(menuid).className="ml active";
-	for (var i=1;i<cq;i++){
-		var x="class"+i;console.log(x);
-		document.getElementById(x).style.color="#fff";
-		document.getElementById(x).style.background="#000";
-	};
-		var x=document.getElementsByClassName("timelist");
+
+function setstyle2(){
+	var manuid="2";document.cookie="style=2";
+		for (var i=0;i<mq;i++){var x="mc"+i;document.getElementById(x).className="ml";};var menuid="mc"+manuid;document.getElementById(menuid).className="ml active";
+		var c="info";var x=document.getElementsByClassName(c);
 		for (var i=0;i<x.length;i++){
 			x[i].style.background="#000";x[i].style.color="#fff";
-		};
+		};document.cookie="style=2";
 };
--- software/js/infos.js
+++ software/js/infos.js
@@ -1,10 +1,17 @@
 //从左到右的数据为：周目、第几节课、科目ID、科目名字、其他信息。
 //如在周五第三节课有一节课为篮球课，编号是cxk，在舞蹈房上课，则是l("5","3","cxk","篮球课","舞蹈房"");
-l("2","1","1","JavaScript","T4203");
+l("1","1","a","Office办公软件","L1-402");
+l("1","2","a","Office办公软件","L1-402");
+l("2","1","b","Java程序设计","T4-302");
+l("3","4","b","Java程序设计","L1-201");
+l("3","1","c","Java程序设计","L1-201");
+l("3","2","c","Java程序设计","L1-201");
+l("4","2","d","体育","操场/球场");
+l("5","2","e","思想道德修养与法律基础","L1-602");
+l("1","4","f","形式与政治","L1-601（单周）");
 
 function l(c,t,n,clsname,intext){
-	var id="info"+c+t,clsid="class"+n;var elm=document.getElementById(id);
-	if (t=="1"){var time="08:00-09:50 ";} else if (t=="2"){var time="10:00-11:40 ";} else if (t=="3"){var time="14:30-16:05 ";} else if (t=="4"){var time="16:15-17:50 ";} else if (t=="5"){var time="18:00~ ";}  else {var time="~ ";};
+	var id="info"+c+t,clsid="info "+n;var elm=document.getElementById(id);if (t=="1"){var time="08:00-09:50 ";} else if (t=="2"){var time="10:00-11:40 ";} else if (t=="3"){var time="14:30-16:05 ";} else if (t=="4"){var time="16:15-17:50 ";} else if (t=="5"){var time="18:00~ ";}  else {var time="~ ";};
 	elm.innerHTML=time+"<div class='intext'>"+clsname+"</div>"+"<div class='intext'>"+intext+"</div>";
-	elm.id=clsid;elm.style.color="#fff"
+	elm.className=clsid;elm.style.color="#fff";elm.style.display="block"
 };
