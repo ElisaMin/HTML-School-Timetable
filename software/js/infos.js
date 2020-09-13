4c21be06b9267a41403a977144327b307e52127b
Heizi <35397814+ElisaMin@users.noreply.github.com>
2020-09-13 09:33:56 +0800

    Update infos.js

--- software/js/infos.js
+++ software/js/infos.js
@@ -44,7 +44,7 @@ l("5","3","f","职业规划","");
 function l(c,t,n,clsname,intext){
 	var id="info"+c+t,clsid="info "+n;var elm=document.getElementById(id);
 	//switch (t){ case 1: time="08:00-09:50 "; break; case 2: time="10:00-11:40 ";break;case 3 : time="14:30-16:05 ";break; case 4 :time="16:15-17:50 "; break; case 5:time="18:00~ ";break;default: time="~ ";};
-	if (t=="1"){var time="08:00-09:50 ";} else if (t=="2"){var time="10:00-11:40 ";} else if (t=="3"){var time="14:30-16:05 ";} else if (t=="4"){var time="16:15-17:50 ";} else if (t=="5"){var time="18:00~ ";}  else {var time="~ ";};
+	if (t=="1"){var time="08:00-09:50 ";} else if (t=="2"){var time="10:00-11:40 ";} else if (t=="3"){var time="14:30-16:05 ";} else if (t=="4"){var time="16:15-17:50 ";} else if (t=="5"){var time="19:00-22:10";}  else {var time="~ ";};
 	elm.innerHTML=time+"<div class='intext'>"+clsname+"</div>"+"<div class='intext'>"+intext+"</div>";
 	elm.className=clsid;elm.style.color="#fff";//elm.style.display="block"
 };
